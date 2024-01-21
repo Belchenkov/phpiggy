6abc876ac8cc17329c518129deaaac729d4b5b56
Алексей Бельченков <u608110@gmail.com>
2024-01-21 17:02:19 +0400

    Make HomeController

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -8,22 +8,38 @@ class Router
 {
     private array $routes = [];
 
-    public function add(string $method, string $path): void
+    public function add(string $method, string $path, array $controller): void
     {
         $path = $this->normalizePath($path);
 
         $this->routes[] = [
             'path' => $path,
             'method' => strtoupper($method),
+            'controller' => $controller,
         ];
+    }
+
+    public function dispatch(string $path, string $method)
+    {
+        $path = $this->normalizePath($path);
+        $method = strtoupper($method);
+
+        foreach ($this->routes as $route) {
+            if (!preg_match("#^{$route['path']}$#", $path) || $route['method'] !== $method) {
+                continue;
+            }
+
+            [$class, $function] = $route['controller'];
+            $controller_instance = new $class;
+            $controller_instance->$function();
+        }
     }
 
     private function normalizePath(string $path): string
     {
         $path = trim($path, '/');
         $path = "/{$path}/";
-        $path = preg_replace('#[/]{2,}#', '/', $path);
 
-        return $path;
+        return preg_replace('#[/]{2,}#', '/', $path);
     }
 }
