07dbbe3592502947e7c925b2251c078a323161da
Алексей Бельченков <u608110@gmail.com>
2024-02-01 09:54:43 +0400

    Clear session errors & display errors

--- src/App/views/register.php
+++ src/App/views/register.php
@@ -3,7 +3,7 @@
 <section
     class="max-w-2xl mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded"
 >
-    <?php var_dump($errors); ?>
+    <?php /**@var array $errors */ ?>
     <form
         action="/register"
         method="POST"
@@ -18,6 +18,9 @@
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="john@example.com"
             />
+            <?php if (array_key_exists('email', $errors)) : ?>
+                <div class="bg-gray-100 mt-2 p-2 text-red-500"><?= e($errors['email'][0]); ?></div>
+            <?php endif; ?>
         </label>
         <!-- Age -->
         <label class="block">
@@ -28,6 +31,9 @@
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder=""
             />
+            <?php if (array_key_exists('age', $errors)) : ?>
+                <div class="bg-gray-100 mt-2 p-2 text-red-500"><?= e($errors['age'][0]); ?></div>
+            <?php endif; ?>
         </label>
         <!-- Country -->
         <label class="block">
@@ -41,6 +47,9 @@
                 <option value="Mexico">Mexico</option>
                 <option value="Invalid">Invalid Country</option>
             </select>
+            <?php if (array_key_exists('country', $errors)) : ?>
+                <div class="bg-gray-100 mt-2 p-2 text-red-500"><?= e($errors['country'][0]); ?></div>
+            <?php endif; ?>
         </label>
         <!-- Social Media URL -->
         <label class="block">
@@ -51,6 +60,9 @@
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder=""
             />
+            <?php if (array_key_exists('socialMediaURL', $errors)) : ?>
+                <div class="bg-gray-100 mt-2 p-2 text-red-500"><?= e($errors['socialMediaURL'][0]); ?></div>
+            <?php endif; ?>
         </label>
         <!-- Password -->
         <label class="block">
@@ -61,6 +73,9 @@
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder=""
             />
+            <?php if (array_key_exists('password', $errors)) : ?>
+                <div class="bg-gray-100 mt-2 p-2 text-red-500"><?= e($errors['password'][0]); ?></div>
+            <?php endif; ?>
         </label>
         <!-- Confirm Password -->
         <label class="block">
@@ -71,6 +86,9 @@
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder=""
             />
+            <?php if (array_key_exists('confirm', $errors)) : ?>
+                <div class="bg-gray-100 mt-2 p-2 text-red-500"><?= e($errors['confirm'][0]); ?></div>
+            <?php endif; ?>
         </label>
         <!-- Terms of Service -->
         <div class="block">
@@ -84,6 +102,9 @@
                         />
                         <span class="ml-2">I accept the terms of service.</span>
                     </label>
+                    <?php if (array_key_exists('accept', $errors)) : ?>
+                        <div class="bg-gray-100 mt-2 p-2 text-red-500"><?= e($errors['accept'][0]); ?></div>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
