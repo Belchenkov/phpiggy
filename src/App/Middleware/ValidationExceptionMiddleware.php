1c6610a8c75c70514020cfc16f7201a3117e3d38
Алексей Бельченков <u608110@gmail.com>
2024-02-03 11:20:34 +0400

    filtering sensitive data

--- src/App/Middleware/ValidationExceptionMiddleware.php
+++ src/App/Middleware/ValidationExceptionMiddleware.php
@@ -15,8 +15,15 @@ class ValidationExceptionMiddleware implements MiddlewareInterface
         try {
             $next();
         } catch (ValidationException $e) {
+            $old_form_data = $_POST;
+            $excluded_fields = ['password', 'confirm'];
+            $formatted_form_data = array_diff_key(
+                $old_form_data,
+                array_flip($excluded_fields)
+            );
+
             $_SESSION['errors'] = $e->errors;
-            $_SESSION['old_form_data'] = $_POST;
+            $_SESSION['old_form_data'] = $formatted_form_data;
 
             $referer = $_SERVER['HTTP_REFERER'];
             redirectTo($referer);
